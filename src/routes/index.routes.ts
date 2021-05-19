4270a36d74ca9ea6d9a5e31d65fe28c084ce43b6
alu0101216775 <alu0101216775@ull.edu.es>
2021-05-19 08:19:03 +0100

    Indice de rutas creado

---
+++ src/routes/index.routes.ts
@@ -0,0 +1,19 @@
+import {Request, Response, Router} from 'express'
+
+class ApiRoutes {
+    router: Router;
+    constructor() {
+        this.router = Router();
+        this.routes();
+    }
+
+    routes() {
+        this.router.get('/', (req, res) => {
+            res.send("To use the API, you must go to: /ingredients /courses or /menus");
+        })
+    }
+}
+
+const apiRoutes = new ApiRoutes();
+apiRoutes.routes();
+export default apiRoutes.router;
