460f301a5a6683f496a6365e00d2dfd6e2ac1580
= <=>
2021-05-22 13:31:15 +0100

    Modelo Plato Creado

---
+++ src/models/Course.ts
@@ -0,0 +1,59 @@
+import * as mongoose from "mongoose";
+
+import {IngredientInterface} from './Ingredient'
+
+export type CourseType = "STARTER" | "FIRSTCOURSE" | "SECONDCOURSE" | "DESSERT";
+
+export interface CourseInterface {
+  name: string;
+  courseType: CourseType;
+  ingredients: { ingredient: IngredientInterface, amountInGrams: number }[];
+  coursePrice: number;
+  courseComposition: { carbohydrates: number, proteins: number, lipids: number };
+  mainNutrient: string;
+}
+
+const CourseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  courseType: {
+    type: String,
+    enum: ["STARTER", "FIRSTCOURSE", "SECONDCOURSE", "DESSERT"],
+    required: true,
+  },
+  coursePrice: {
+      type: Number,
+      required: true
+  },
+  courseComposition: {
+    carbohydrates: {
+        type: Number,
+        required: true,
+      }, 
+      proteins: {
+        type: Number,
+        required: true,
+      }, 
+      lipids: {
+        type: Number,
+        required: true,
+      }
+  },
+  ingredients: {                    
+    type: [{
+      ingredient: {
+        type: String,
+        required: true,
+      }, 
+      amountInGrams: {
+        type: Number,
+        required: true,
+      }
+    }]   
+  }
+});
+
+const Course = mongoose.model<CourseInterface>("Course", CourseSchema);
+export default Course;
---
+++ src/models/Ingredient.ts
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose'
+
+export type ingredientType = "CARNES-HUEVOS-LEGUMBRES" | "VERDURAS-HORTALIZAS" | "LACTEOS" | "CEREALES" | "FRUTAS";
+
+export interface IngredientInterface {
+    name: string;
+    location: string;
+    ingredientGroup: ingredientType;
+    nutrients: { carbohydrates: number, proteins: number, lipids: number };
+    pricePerKg: number;
+}
+
+const IngredientSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    ingredientGroup: {
+      type: String,
+      enum: ["CARNES-HUEVOS-LEGUMBRES", "VERDURAS-HORTALIZAS", "LACTEOS", "CEREALES", "FRUTAS"],
+      required: true,
+    },
+    nutrients: {
+      carbohydrates: {
+        type: Number,
+        required: true,
+      }, 
+      proteins: {
+        type: Number,
+        required: true,
+      }, 
+      lipids: {
+        type: Number,
+        required: true,
+      }, 
+    },
+    pricePerKg: {
+      type: Number, 
+      required: true,
+    }
+  });
+  
+  const Ingredient = mongoose.model<IngredientInterface>("Ingredient", IngredientSchema);
+  export default Ingredient;
